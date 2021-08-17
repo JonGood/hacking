87ce15b2c1376be4763adcc66db965b042d26cac
th3suit <mainframemadness@gmail.com>
2021-08-17 14:06:11 -0700

    Update install_hacking_tools.sh

--- install_hacking_tools.sh
+++ install_hacking_tools.sh
@@ -11,20 +11,32 @@ echo " "
 # Install Terminator.
 echo "Installing Terminator..."
 sudo apt-get install terminator
+echo " "
+echo " "
 
 # Update local exploits from exploit-db.
 echo "Updating searchsploit exploits..."
 sudo searchsploit --update
+echo " "
+echo " "
 
+
+# ::REMOTE Executed Tools (On Kali / Attacker)::
 # AUTORECON: Remove old instance and download fresh.
 echo "Removing old version of AutoRecon..."
 sudo rm -r /opt/remote/AutoRecon
 echo "Downloading AutoRecon..."
 sudo git clone https://github.com/Tib3rius/AutoRecon.git /opt/remote/AutoRecon
+echo " "
+echo " "
 
+
+# ::LOCAL Executed Tools (On Victim)::
 # PEASS (WinPEAS / LinPEAS): Remove old instance and download fresh instances of enumeration scripts.
 echo "Removing old version of PEASS-ng"
 sudo rm -r /opt/local/PEASS-ng
 echo "Downloading PEASS-ng..."
 sudo git clone https://github.com/carlospolop/PEASS-ng.git /opt/local/PEASS-ng
+echo " "
+echo " "
 
